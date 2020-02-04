a855d3f12085038d1018b4f4aba8fe12173f080c
Jay Koutavas <jay@heynow.com>
2020-02-04 15:25:14 -0500

    Initial commit: create a macOS document based Swift app that uses a Storyboard

---
+++ reswift-jobs/reswift-jobs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  reswift-jobs
+//
+//  Created by Jay Koutavas on 2/4/20.
+//  Copyright © 2020 Heynow Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ reswift-jobs/reswift-jobs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  reswift-jobs
+//
+//  Created by Jay Koutavas on 2/4/20.
+//  Copyright © 2020 Heynow Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
